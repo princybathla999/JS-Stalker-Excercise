3228ecdfbcd027156f4eae7700fcd6c480841390
Alpha Blog <princybathla999@yahoo.com>
2017-08-04 15:05:14 -0400

    JS Stalker Exercise

---
+++ app.js
@@ -0,0 +1,7 @@
+var first_name= prompt("What is your first name?");
+var last_name = prompt("What is your last name?");
+var age = prompt("What is your age?");
+alert("Your full name is:"+first_name +" "+last_name );
+console.log("Your full name is:"+first_name +" "+last_name );
+alert("You are "+age +"years old.");
+console.log("You are "+age +"years old.");
